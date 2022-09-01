818ec126fc50e5349bf8fa61a320eed52dec4db5
Ferve <ferve91@gmail.com>
2022-09-01 08:04:38 -0300

    Pequeños cambios generales

--- biblioteca-virtual/src/pages/index.js
+++ biblioteca-virtual/src/pages/index.js
@@ -1,6 +1,6 @@
 import { useAppContext } from "../store/store";
 import Book from "../components/book";
-import { Link } from "react-router-dom";
+
 import Layout from "../components/layout";
 
 export default function Index() {
